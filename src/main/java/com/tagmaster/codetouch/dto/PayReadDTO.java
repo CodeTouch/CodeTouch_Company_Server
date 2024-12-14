dfd84a56b4fa0f97b7a40d02c30ea7119ab52511
heejunida <qazwsx05723@gmail.com>
2024-12-14 09:21:23 +0900

    12월 14일 오후 9시 21분
    결제 내역 읽어오기

--- src/main/java/com/tagmaster/codetouch/dto/PayReadDTO.java
+++ src/main/java/com/tagmaster/codetouch/dto/PayReadDTO.java
@@ -1,9 +1,10 @@
 package com.tagmaster.codetouch.dto;
 
 import com.tagmaster.codetouch.entity.company.CompanyUser;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -11,5 +12,5 @@ public class PayReadDTO {
     private CompanyUser user;
     private String siteName;
     private String merchantId;
-
+    private LocalDateTime createAt;
 }
