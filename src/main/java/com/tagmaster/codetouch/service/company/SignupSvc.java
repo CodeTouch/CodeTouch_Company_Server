d21c7fa6912001ea5c207bbbba0891cd95462d43
heejunida <qazwsx05723@gmail.com>
2024-12-15 21:51:16 +0900

    12월 15일 오후 09시 50분
    리펙토링 / 결제내역조회

---
+++ src/main/java/com/tagmaster/codetouch/service/company/SignupSvc.java
@@ -0,0 +1,54 @@
+package com.tagmaster.codetouch.service.company;
+
+import com.tagmaster.codetouch.dto.company.SignupDTO;
+import com.tagmaster.codetouch.entity.company.CompanyUser;
+import com.tagmaster.codetouch.entity.customer.CustomerUser;
+import com.tagmaster.codetouch.repository.company.CompanyUserRepo;
+import com.tagmaster.codetouch.repository.customer.CustomerUserRepo;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@Service
+public class SignupSvc {
+    private final CompanyUserRepo companyUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public SignupSvc(
+            CompanyUserRepo companyUserRepository,
+            BCryptPasswordEncoder bCryptPasswordEncoder
+    ) {
+        this.companyUserRepository = companyUserRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Transactional/*("chainedTransactionManager")*/
+    public void SignupProcess(SignupDTO signupDTO) {
+        String email = signupDTO.getEmail();
+
+        Boolean isExist = companyUserRepository.existsByEmail(email);
+
+        if (isExist) {
+
+            return;
+        }
+
+        CompanyUser companyUser = new CompanyUser();
+
+        companyUser.setNickname(signupDTO.getNickname());
+        companyUser.setPassword(bCryptPasswordEncoder.encode(signupDTO.getPassword()));
+        companyUser.setName(signupDTO.getName());
+        companyUser.setPhone(signupDTO.getPhone());
+        companyUser.setEmail(email);
+        companyUser.setGender(signupDTO.getGender());
+        companyUser.setBirth(LocalDate.parse(signupDTO.getBirth()));
+
+        companyUserRepository.save(companyUser);
+    }
+
+    public Boolean isEmailAvailable(String email){
+        return !companyUserRepository.existsByEmail(email);
+    }
+}
